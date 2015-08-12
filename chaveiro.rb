5d91cebf3bfcaa1b46dddc68cfa892e97cf790af
UserX <userx.gnu@gmail.com>
2015-08-11 21:56:33 -0300

    port number update

--- chaveiro.rb
+++ chaveiro.rb
@@ -7,7 +7,8 @@ require 'timeout'
 print "IP Address: "
 ip = gets.chomp
 
-ports=1..15000
+# o numero maximo de portas do computador é 65536 ( 2^16 - Ou seja, 16 bits de portas)
+ports=1..65536
 ports.each do |scan|
 
 #Ver uma solução Alex
